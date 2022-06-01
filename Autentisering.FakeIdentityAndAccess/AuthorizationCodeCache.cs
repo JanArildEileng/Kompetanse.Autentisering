ed02973717ec495022df7953325d966013013efe
Jan Eileng <janarild.eileng@fhi.no>
2022-06-01 15:15:16 +0200

    Refactor AuthorizationCodeCache

---
+++ Autentisering.FakeIdentityAndAccess/AuthorizationCodeCache.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+
+
+namespace Autentisering.FakeIdentityAndAccess;
+
+public class AuthorizationCodeCache
+{
+    private MemoryCache cache { get; set; }
+    private MemoryCacheEntryOptions cacheEntryOptions;
+
+    public AuthorizationCodeCache()
+    {
+        cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetAbsoluteExpiration(DateTime.Now.AddMinutes(5)); 
+
+        cache = new MemoryCache(new MemoryCacheOptions
+        {
+        });
+    }
+
+    public void Set(string authorizationCode, AuthorizationCodeContent authorizationCodeContent)
+    {
+        cache.Set(authorizationCode, authorizationCodeContent, this.cacheEntryOptions);
+    }
+
+    public bool TryGet(string authorizationCode, out AuthorizationCodeContent authorizationCodeContent)
+    {
+        return cache.TryGetValue(authorizationCode, out authorizationCodeContent);
+    }
+}
