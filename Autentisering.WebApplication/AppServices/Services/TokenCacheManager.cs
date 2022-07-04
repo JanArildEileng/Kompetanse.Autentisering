bd848f071fdc1c59a2b3dfbffb52bf9518749a29
Jan Arild Eileng <JanArild.Eileng@fhi.no>
2022-07-04 07:59:02 +0200

    TokenFreshService Bugfix key

--- Autentisering.WebApplication/AppServices/Services/TokenCacheManager.cs
+++ Autentisering.WebApplication/AppServices/Services/TokenCacheManager.cs
@@ -6,10 +6,7 @@ namespace Authorization.WebBFFApplication.AppServices.Services
     {
         private readonly ILogger<TokenCacheManager> logger;
         private readonly IMemoryCache memoryCache;
-  
-        private string AccessKey(string username) => $"AccessToken:{username}";
-        private string RefreshKey(string username) => $"RefreshToken:{username}";
-
+ 
         public TokenCacheManager(ILogger<TokenCacheManager> logger, IMemoryCache memoryCache)
         {
             this.logger = logger;
@@ -19,6 +16,12 @@ namespace Authorization.WebBFFApplication.AppServices.Services
 
         public void SetToken(string username, string accessToken, string refreshToken)
         {
+            string AccessKey = $"AccessToken:{username}";
+            string RefreshKey = $"RefreshToken:{username}";
+
+            logger.LogInformation("Set Token for {username}  AccessKey={AccessKey}  RefreshKey={RefreshKey}", username, AccessKey, RefreshKey);
+
+
             if (accessToken != null)
             {
                 logger.LogInformation("Add token to memoryCache");
@@ -34,8 +37,17 @@ namespace Authorization.WebBFFApplication.AppServices.Services
 
         public async Task<(string, string)> GetToken(string username)
         {
+            string AccessKey = $"AccessToken:{username}";
+            string RefreshKey= $"RefreshToken:{username}";
+
+
             string accessToken;
             string refreshToken;
+
+
+            logger.LogInformation("Get Token for {username}  AccessKey={AccessKey}  RefreshKey={RefreshKey}", username, AccessKey, RefreshKey);
+
+
             memoryCache.TryGetValue(AccessKey, out accessToken);
             memoryCache.TryGetValue(RefreshKey, out refreshToken);
 
